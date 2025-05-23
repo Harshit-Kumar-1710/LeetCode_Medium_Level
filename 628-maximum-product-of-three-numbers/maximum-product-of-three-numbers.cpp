18604341e69ad04144eedd008d215adc356e4dfc
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-05-23 20:37:33 +0530

    Time: 21 ms (11.12%) | Memory: 31.6 MB (42.83%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -4,9 +4,6 @@ public:
     {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        int Max1=nums[n-1]*nums[0]*nums[1];
-        int Max2=nums[n-1]*nums[n-2]*nums[n-3];
-        Max1=max(Max1,Max2);
-        return Max1;
+        return max(nums[0]*nums[1]*nums[n-1],nums[n-1]*nums[n-2]*nums[n-3]);
     }
 };
