eeabb419a810f07312ec0aed5a2ae41b07472476
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-08-30 18:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.69%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    vector<int>Vec;
+    vector<int>Dp;
     int Func(int n)
     {
-        if(n<=2)
-        return n;
-        int a,b;
-        if(Vec[n-1]!=-1) a=Vec[n-1];
-        else a=Func(n-1);
-        if(Vec[n-2]!=-1)
-            b=Vec[n-2];
-        else b=Func(n-2);
-        Vec[n]=a+b;
-        return Vec[n];
+        if(n<=2) return n;
+        if(Dp[n]!=-1) return Dp[n];
+        return Dp[n]=Func(n-1)+Func(n-2);
     }
     int climbStairs(int n) 
     {
-        Vec.resize(n+1,-1);
+        Dp.resize(n+1,-1);
         return Func(n);
-        
     }
 };
