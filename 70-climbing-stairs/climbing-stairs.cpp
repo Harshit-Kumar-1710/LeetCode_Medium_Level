5a66457db6c82a788687de338ceda52d7c53780f
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-09-18 14:02:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.81%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,18 +3,15 @@ public:
     vector<int>Dp;
     int Func(int n)
     {
-        if(n<=2) return n;
-        int a,b;
-        if(Dp[n-1]!=-1) a=Dp[n-1];
-        else a=Func(n-1);
-        if(Dp[n-2]!=-1) b=Dp[n-2];
-        else b=Func(n-2);
-        Dp[n]=a+b;
-        return Dp[n];
+        if(n==1) return 1;
+        if(n==0) return 1;
+        if(Dp[n]!=-1) return Dp[n];
+        return Dp[n]=Func(n-1)+Func(n-2);
     }
     int climbStairs(int n) 
     {
         Dp.resize(n+1,-1);
         return Func(n);
+        
     }
 };
