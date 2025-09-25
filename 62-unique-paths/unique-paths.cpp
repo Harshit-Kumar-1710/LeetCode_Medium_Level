9ee80239708776d8c5e7435b9a792f6c0d63a66d
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-09-25 22:06:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.96%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -6,9 +6,9 @@ public:
         if(i>=m || j>=n) return 0;
         if(Dp[i][j]!=-1) return Dp[i][j];
         if(i==m-1 && j==n-1) return 1;
+        int Bottom=Func(i+1,j,m,n);
         int Right=Func(i,j+1,m,n);
-        int Down=Func(i+1,j,m,n);
-        return Dp[i][j]=Right+Down;
+        return Dp[i][j]=Bottom+Right;
     }
     int uniquePaths(int m, int n) 
     {
