cb8b6f7d7a9624645cf46a8573e4ad873f52b6f1
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-09-26 21:06:29 +0530

    Time: 4 ms (16.79%) | Memory: 16.4 MB (41.12%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -4,14 +4,11 @@ public:
     int Func(int i,int j,int n,int m,vector<vector<int>>&grid)
     {
         if(i>=n || j>=m) return 1e9;
-        else if(i==n-1 && j==m-1)
-        {
-            return grid[i][j];
-        }
-        else if(Dp[i][j]!=-1) return Dp[i][j];
-        int p=grid[i][j]+Func(i+1,j,n,m,grid);
-        int q=grid[i][j]+Func(i,j+1,n,m,grid);
-        return Dp[i][j]=min(p,q);
+        if(i==n-1 && j==m-1) return grid[i][j];
+        if(Dp[i][j]!=-1) return Dp[i][j];
+        int Bottom=grid[i][j]+Func(i,j+1,n,m,grid);
+        int Right=grid[i][j]+Func(i+1,j,n,m,grid);
+        return Dp[i][j]=min(Bottom,Right);
     }
     int minPathSum(vector<vector<int>>& grid) 
     {
