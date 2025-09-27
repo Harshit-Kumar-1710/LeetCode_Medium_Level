729c7a9bc1204bbf85f84e757a4021d60938e5c1
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-09-27 18:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (48.68%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -3,13 +3,19 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) 
     {
         int n=gas.size(),TotalGas=0,TotalCost=0;
+        for(auto val:gas)
+        {
+            TotalGas+=val;
+        }
+        for(auto val:cost)
+        {
+            TotalCost+=val;
+        }
         if(TotalCost>TotalGas) return -1;
         int CurrentInd=0,CurrentGas=0;
         // Only a Unique Solution exists.
         for(int i=0;i<n;i++)
         {
-            TotalGas+=gas[i];
-            TotalCost+=cost[i];
             CurrentGas+=(gas[i]-cost[i]);
             if(CurrentGas<0)
             {
@@ -17,7 +23,6 @@ public:
                 CurrentGas=0;
             }
         }
-        if(TotalCost>TotalGas) return -1;
         return CurrentInd;
         
     }
