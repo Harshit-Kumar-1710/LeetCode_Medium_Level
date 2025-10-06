63e0103bb7182f217b4f67baac8a7882f248829b
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-10-06 17:33:39 +0530

    Time: 10 ms (70.84%) | Memory: 16.9 MB (40.65%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -3,27 +3,23 @@ public:
     vector<vector<int>>Dp;
     int Func(int i,int j,string &word1,string &word2)
     {
-        if(i<0)
+        if(i<0 || j<0)
         {
-            return word2.substr(0,j+1).length();
-        }
-        if(j<0)
-        {
-            return word1.substr(0,i+1).length();
+            return 0;
         }
         if(Dp[i][j]!=-1) return Dp[i][j];
         if(word1[i]==word2[j])
         {
-            return Dp[i][j]=Func(i-1,j-1,word1,word2);
+            return Dp[i][j]=1+Func(i-1,j-1,word1,word2);
         }
-        return Dp[i][j]=min(1+Func(i-1,j,word1,word2),1+Func(i,j-1,word1,word2));
+        return Dp[i][j]=max(Func(i-1,j,word1,word2),Func(i,j-1,word1,word2));
     }
     int minDistance(string word1, string word2) 
     {
         int n=word1.size();
         int m=word2.size();
         Dp.resize(n,vector<int>(m,-1));
-        return Func(n-1,m-1,word1,word2);
-        
+        int k=Func(n-1,m-1,word1,word2);
+        return (n-k)+(m-k);
     }
 };
