bf83ce20b238943f05bc6957d3e2a79f5b4980ea
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-10-06 12:14:50 +0530

    Time: 42 ms (27.09%) | Memory: 27.8 MB (6.21%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<vector<int>>Dp;
-    int Func(int i,int j,string&s1,string&s2)
+    int LCS(int i,int j,string &t1,string &t2)
     {
         if(i<0 || j<0) return 0;
         if(Dp[i][j]!=-1) return Dp[i][j];
-        if(s1[i]==s2[j]) Dp[i][j]=1+Func(i-1,j-1,s1,s2);
-        else Dp[i][j]=max(Func(i-1,j,s1,s2),Func(i,j-1,s1,s2));
-        return Dp[i][j];
+        if(t1[i]==t2[j]) return Dp[i][j]=1+LCS(i-1,j-1,t1,t2);
+        else return Dp[i][j]=max(LCS(i-1,j,t1,t2),LCS(i,j-1,t1,t2));
     }
     int longestCommonSubsequence(string text1, string text2) 
     {
-        Dp.resize(text1.size(),vector<int>(text2.size(),-1));
-        return Func(text1.size()-1,text2.size()-1,text1,text2);
+        int n=text1.size();
+        int m=text2.size();
+        Dp.resize(n,vector<int>(m,-1));
+        return LCS(n-1,m-1,text1,text2);
     }
 };
