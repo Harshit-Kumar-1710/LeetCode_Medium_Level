47f9ce66a029e5ee6081c235315d679de347b2fb
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-10-27 20:07:29 +0530

    Time: 3 ms (30.60%) | Memory: 15.8 MB (8.12%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>>Vvi;
+    void Func(int i,int n,vector<int>&nums,vector<int>Vec)
+    {
+        if(i==n) 
+        {
+            Vvi.push_back(Vec);
+            return;
+        }
+        Vec.push_back(nums[i]);
+        Func(i+1,n,nums,Vec);
+        Vec.pop_back();
+        Func(i+1,n,nums,Vec);
+        return;
+    } 
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int>Vec;
+        Func(0,n,nums,Vec);
+        return Vvi;      
+    }
+};
