3e0cd7b607ae234ea989c009b8944faa38477963
Harshit Kumar <144518682+Harshit-Kumar-1710@users.noreply.github.com>
2025-10-29 08:29:29 +0530

    Time: 4 ms (6.26%) | Memory: 12.1 MB (5.01%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
+    int Func(int i,int n,string s,int Sign,long long num,bool SignUsed,bool started)
+    {
+        if(i==n) return (int)(Sign*num);
+        if(s[i]==' ' && !started && !SignUsed) return Func(i+1,n,s,Sign,num,SignUsed,started);
+        if(!started && !SignUsed && (s[i]=='-' || s[i]=='+')) 
+        {
+            SignUsed=true;
+            if(s[i]=='-') return Func(i+1,n,s,-1,num,SignUsed,started);
+            else return Func(i+1,n,s,Sign,num,SignUsed,started);
+        }
+        if(s[i]>='0' && s[i]<='9')
+        {
+            started=true;
+            num=num*10+(s[i]-'0');
+            if(Sign==1 && num>INT_MAX) return INT_MAX;
+            if(Sign==-1 && -num<INT_MIN) return INT_MIN;
+            return Func(i+1,n,s,Sign,num,SignUsed,started);
+        }
+        return (int)(Sign*num);
+    }
     int myAtoi(string s) 
     {
-        int n=s.size();
-        int i=0,Negative=1;
-        long long Num=0;
-        while(i<n && s[i]==' ')
-        {
-            i++;
-        }
-        if(i<n && (s[i]=='-' || s[i]=='+'))
-        {
-            if(s[i]=='-') Negative=-1;
-            i++;
-        }
-                while(i<n && s[i]>='0' && s[i]<='9')
-                {
-                    Num=Num*10+(s[i]-'0');
-                    if(Negative==1 && Num>INT_MAX)
-                    {
-                        return INT_MAX;
-                    }
-                    else if(Negative==-1 && -Num<INT_MIN)
-                    {
-                        return INT_MIN;
-                    }
-                i++;
-                }
-    return (int)(Num*Negative);
+        int n=s.size(),Sign=1;
+        long long num=0;
+        return Func(0,n,s,Sign,num,false,false);
     }
 };
